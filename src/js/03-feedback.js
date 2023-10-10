867d1c1141326bc94a87a1f20690dde921e29454
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-10-11 01:54:14 +0300

    modifi feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,31 @@
+import throttle from 'lodash.throttle';
+
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
 const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
-
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
-
-function loadFromLocalStorage() {
+const saveToLocalStorage = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}, 500);
+const loadFromLocalStorage = () => {
   const savedData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
   if (savedData) {
     emailInput.value = savedData.email;
     messageTextarea.value = savedData.message;
   }
-}
-
-feedbackForm.addEventListener('input', () => {
-  const formData = { email: emailInput.value, message: messageTextarea.value };
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
-});
-
+};
+feedbackForm.addEventListener('input', saveToLocalStorage);
 document.addEventListener('DOMContentLoaded', loadFromLocalStorage);
-
 feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
-  const formData = { email: emailInput.value, message: messageTextarea.value };
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
   localStorage.removeItem(LOCAL_STORAGE_KEY);
   emailInput.value = '';
   messageTextarea.value = '';
